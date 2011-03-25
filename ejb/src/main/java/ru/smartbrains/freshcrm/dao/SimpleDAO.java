8f74ce34c87c51bf57cdc2b0fa221a04fad25967
Alexey Elkin <a.elkin@kamis.ru>
2011-03-25 16:16:32 +0300

    [+] FCRM-13 - Добавить поддержку JPA

--- ejb/src/main/java/ru/smartbrains/freshcrm/dao/SimpleDAO.java
+++ ejb/src/main/java/ru/smartbrains/freshcrm/dao/SimpleDAO.java
@@ -1,7 +1,13 @@
 package ru.smartbrains.freshcrm.dao;
 
+import ru.smartbrains.freshcrm.domain.Simple;
+
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
 
 /**
  * User: Alexey Elkin <elkin.alexey@gmail.com>
@@ -10,4 +16,12 @@ import javax.inject.Named;
 @Named
 @ApplicationScoped
 public class SimpleDAO {
+
+	@PersistenceContext
+	private EntityManager em;
+
+	public List<Simple> getSimpleList() {
+		return em.createQuery("from Simple").getResultList();
+	}
+
 }
--- ejb/src/main/java/ru/smartbrains/freshcrm/services/SimpleService.java
+++ ejb/src/main/java/ru/smartbrains/freshcrm/services/SimpleService.java
@@ -1,6 +1,10 @@
 package ru.smartbrains.freshcrm.services;
 
+import ru.smartbrains.freshcrm.dao.SimpleDAO;
+import ru.smartbrains.freshcrm.domain.Simple;
+
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 import java.io.Serializable;
 
 /**
@@ -12,7 +16,16 @@ public class SimpleService implements Serializable {
 
 	private static final long serialVersionUID = -1036304210641465757L;
 
+	@Inject
+	private SimpleDAO simpleDAO;
+
 	public String getHello() {
-		return "Hello world!";
+		StringBuffer buffer = new StringBuffer();
+		buffer.append("| id | name |\n");
+		for (Simple simple : simpleDAO.getSimpleList()) {
+			buffer.append("|").append(simple.getId()).append(" | ").append(simple.getName()).append("\n");
+		}
+
+		return "Hello world!\n" + buffer.toString();
 	}
 }
